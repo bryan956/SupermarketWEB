53429234d54db6f7043d2ef4c1e3bceb4ab3a98f
Estudiantes <Estudiantes@cecep.edu.co>
2023-05-10 17:50:58 -0500

    Index Productos

--- Data/SupermarketContext.cs
+++ Data/SupermarketContext.cs
@@ -13,5 +13,6 @@ namespace SupermarketWEB.Data
         public DbSet<Category> Categories { get;set; }
         public DbSet<Provider> Providers { get; set; }
         public DbSet<PayMode> PayModes { get; set; }
+        public DbSet<Producto> Productos { get; set; }
     }
 }
---
+++ Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace SupermarketWEB.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string? Description { get; set; }
+        public ICollection<Product>? Products { get; set; } = default!;
+    }
+}
