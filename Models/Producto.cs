484fff2be9b9a3e0f394a5e1742cff27d12b65bb
Estudiantes <Estudiantes@cecep.edu.co>
2023-05-10 18:13:40 -0500

    Create Productos

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,10 +1,14 @@
-﻿namespace SupermarketWEB.Models
+﻿using Microsoft.Xrm.Sdk;
+using System.Data.SqlTypes;
+
+namespace SupermarketWEB.Models
 {
     public class Producto
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Price { get; set; }
+        public Decimal Price { get; set; }
+        public int Amount { get; set; }
         public string? Description { get; set; }
         public ICollection<Product>? Products { get; set; } = default!;
     }
--- Pages/Productos/Create.cshtml.cs
+++ Pages/Productos/Create.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.Productos
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly SupermarketContext _context;
+
+        public CreateModel(SupermarketContext context)
         {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Producto Producto { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Productos == null || Producto == null)
+            {
+                return Page();
+            }
+
+            _context.Productos.Add(Producto);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
