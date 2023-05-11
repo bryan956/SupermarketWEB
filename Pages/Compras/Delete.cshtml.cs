40d97e4b978b71be8ea848bfcf3959eb6e3505d0
bryan956 <rodriguezab956@gmail.com>
2023-05-11 18:10:18 -0500

    Create Compra

--- Pages/Compras/Delete.cshtml.cs
+++ Pages/Compras/Delete.cshtml.cs
@@ -1,12 +1,60 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.Compras
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        public readonly SupermarketContext _context;
+
+        public DeleteModel(SupermarketContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Compra Compra { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Compras == null)
+            {
+                return NotFound();
+            }
+
+            var compra = await _context.Compras.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (compra == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Compra = compra;
+            }
+            return Page();
+        }
+
+
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Compras == null)
+            {
+                return NotFound();
+            }
+            var compra = await _context.Compras.FindAsync(id);
+
+            if (compra != null)
+            {
+                Compra = compra;
+                _context.Compras.Remove(compra);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage("./Index");
         }
     }
 }
