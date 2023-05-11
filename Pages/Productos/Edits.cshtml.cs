3006f2d7b6137a77f3c36c7a1de21ed84913fca9
bryan956 <rodriguezab956@gmail.com>
2023-05-11 10:54:53 -0500

    Edit Productos

---
+++ Pages/Productos/Edits.cshtml.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Productos
+{
+    public class EditsModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public EditsModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Producto Producto { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Productos == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos.FirstOrDefaultAsync(m => m.Id == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            Producto = producto;
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Producto).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductoExists(Producto.Id))
+                {
+                    return Page();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ProductoExists(int id)
+        {
+            return (_context.Productos?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+    }
+}
