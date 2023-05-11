3a946af8976c5766e87855d50882b43aa45edd9a
bryan956 <rodriguezab956@gmail.com>
2023-05-11 17:03:29 -0500

    Index Compra y venta

--- Models/ShopSell.cs
+++ Models/ShopSell.cs
@@ -1,11 +1,12 @@
 ﻿namespace SupermarketWEB.Models
 {
-    public class Compra
+    public class ShopSell
     {
         public int Id { get; set; }
+        public string Status { get; set; }
         public string Name { get; set; }
-        public Decimal Price { get; set; }
-        public int Amount { get; set; }
+        public float Price_Xuni { get; set; }
+        public float Amount { get; set; }
         public string? Description { get; set; }
         public ICollection<Product>? Products { get; set; } = default!;
     }
